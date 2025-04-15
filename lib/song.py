d545d9ddc4529d530e4aa4d66335a58d458d2730
Fiona <fionakanyi0@gmail.com>
2025-04-15 21:44:30 +0300

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,42 +1,51 @@
 class Song:
+    # Class attributes
     count = 0
-    artist = []
     genres = []
+    artists = []
     genre_count = {}
     artist_count = {}
-    
+
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-
-        self.add_song_to_count()
-        self.add_to_artists()
-        self.add_to_genres()
-        self.add_to_genre_count()
-        self.add_to_artist_count()
-
-        @classmethod
-        def add_song_to_count(cls):
-            cls.count += 1
-
-        def add_to_artists(self):
-            if self.artist not in Song.artist:
-                Song.artist.append(self.artist)
-
-        def add_to_genres(self):
-            if self.genre not in Song.genres:
-                Song.genres.append(self.genre)
-
-        def add_to_genre_count(self):
-            if self.genre in Song.genre_count:
-                Song.genre_count[self.genre] += 1
-            else:
-                Song.genre_count[self.genre] = 1
-
-        def add_to_artist_count(self):
-            if self.artist in Song.artist_count:
-                Song.artist_count[self.artist] += 1
-            else:
-                Song.artist_count[self.artist] = 1
         
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        """Increments the count of songs created."""
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        """Adds unique genres to the genres list."""
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        """Adds unique artists to the artists list."""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        """Adds to the count of songs per genre."""
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        """Adds to the count of songs per artist."""
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
